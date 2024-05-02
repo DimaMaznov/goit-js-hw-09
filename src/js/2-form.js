b4aad6584f7dc46842714a790023577e7f67b760
DimaMaznov <maznovdmitriy01@gmail.com>
2024-05-02 23:32:14 +0300

    123

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,7 +1,30 @@
-const formData = { email: "", message: "" };
+let formData = { email: "", message: "" };
 const form = document.querySelector(".feedback-form");
+const emailInput = form.querySelector("[name='email']");
+const messageInput = form.querySelector("[name='message']");
 
-form.addEventListener  ("input", Formfocus)
+const savedState = localStorage.getItem("feedback-form-state");
+if (savedState) {
+    formData = JSON.parse(savedState);
+    emailInput.value = formData.email;
+    messageInput.value = formData.message;
+}
 
-function  Formfocus () {
-    localStorage.setItem(localStorageKey, evt.target.value);}
+form.addEventListener("input", (event) => {
+    const name = event.target.name;
+    formData[name] = event.target.value.trim();
+    localStorage.setItem(savedState, JSON.stringify(formData));
+});
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+   
+    if (emailInput.value.trim() === "" || messageInput.value.trim() === "") {
+        return alert("Fill please all fields");
+    }
+
+    console.log("Form Data:", formData);
+    localStorage.removeItem("feedback-form-state");
+    formData = { email: "", message: "" };
+    form.reset();
+});
